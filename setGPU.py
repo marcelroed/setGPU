ad25ff140e911debbe8164e57c4008fa0eb39bd6
Marcel Rød <marcelroed@gmail.com>
2022-05-25 11:00:27 +0100

    Finish reimplementation and bump version number

--- setGPU.py
+++ setGPU.py
@@ -2,13 +2,18 @@ import os
 import gpustat
 import random
 
-stats = gpustat.GPUStatCollection.new_query()
-ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
-pairs = list(zip(ids, ratios))
-random.shuffle(pairs)
-bestGPU = min(pairs, key=lambda x: x[1])[0]
+bestGPUs = None
 
-print("setGPU: Setting GPU to: {}".format(bestGPU))
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
+def init(n=1):
+    stats = gpustat.GPUStatCollection.new_query()
+    ids = map(lambda gpu: int(gpu.entry['index']), stats)
+    ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
+    pairs = list(zip(ids, ratios))
+    random.shuffle(pairs)
+
+    global bestGPUs
+    bestGPUs = list(map(lambda x: x[0], sorted(pairs, key=lambda x: x[1])[0:n]))
+
+    print("setGPU: Setting visible GPUs to to: {}".format(bestGPUs))
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
