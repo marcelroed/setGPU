f0027e83a91360e0f8efc9d8d70dc9710c07199a
Marcel Rød <marcelroed@gmail.com>
2022-05-25 11:08:20 +0100

    Export stats and number of GPUs

--- setGPU.py
+++ setGPU.py
@@ -3,17 +3,23 @@ import gpustat
 import random
 
 bestGPUs = None
+stats = None
+n_gpus = None
 
 def init(n=1):
+    global bestGPUs, stats, n_gpus
+
     stats = gpustat.GPUStatCollection.new_query()
     ids = map(lambda gpu: int(gpu.entry['index']), stats)
     ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
     pairs = list(zip(ids, ratios))
     random.shuffle(pairs)
 
-    global bestGPUs
     bestGPUs = list(map(lambda x: x[0], sorted(pairs, key=lambda x: x[1])[0:n]))
 
     print("setGPU: Setting visible GPUs to to: {}".format(bestGPUs))
     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
     os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
+
+    _has_inited = True
+    n_gpus = n
